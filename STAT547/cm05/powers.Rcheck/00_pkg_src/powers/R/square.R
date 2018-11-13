fd0cdfc185191bcdc1b646cff013b1ae93189135
Chenchen Guo <756775098@qq.com>
2018-11-13 09:38:53 -0800

    add on

---
+++ STAT547/cm05/powers.Rcheck/00_pkg_src/powers/R/square.R
@@ -0,0 +1,5 @@
+#' @title Square the input
+#' @param x Vector of the numerics
+#' @return The vector x, squared.
+#' @export
+square <- function(x) x^2
---
+++ STAT547/cm05/powers/R/square.R
@@ -0,0 +1,6 @@
+#' @title Square the input
+#' @param x Vector of the numerics
+#' @return The vector x, squared.
+#' @rdname common_doc
+#' @export
+square <- function(x) pow(x, 2)
